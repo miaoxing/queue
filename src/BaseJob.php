feafc813f670d86a06ed410c1a935a4ec13743db
twinh <twinhuang@qq.com>
2020-03-11 11:35:55 +0800

    refactor: 在调用 laravel 相关功能时才启动

--- src/BaseJob.php
+++ src/BaseJob.php
@@ -7,8 +7,14 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Miaoxing\Services\Service\Laravel;
 
 class BaseJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct()
+    {
+        wei()->laravel->bootstrap();
+    }
 }
